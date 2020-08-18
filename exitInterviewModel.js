2993fb26bb1f1147129535bb3b1c0f3de1937f84
ahassard <45524855+ahassard@users.noreply.github.com>
2020-08-18 15:47:38 -0400

    Add files via upload

---
+++ exitInterviewModel.js
@@ -0,0 +1,23 @@
+export default class exitInterview {
+  constructor() {
+    this.resetData();
+  }
+
+  resetData() {
+    this.brandiId = null;
+    this.exitId = null;
+    this.optionFortyCode = null;
+    this.transId = null;
+    this.supplements = null;
+    this.factors = null;
+    this.influence = null;
+    this.outsideInfluence = null;
+    this.otherTraining = null;
+    this.basicBail = null;
+    this.performance = null;
+    this.failures = null;
+    this.diagnosis = null;
+    this.injuryTime = null;
+    this.failureFactors = null;
+  }
+}
---
+++ graduationModel.js
@@ -0,0 +1,12 @@
+export default class graduationData {
+  constructor() {
+    this.resetData();
+  }
+
+  resetData() {
+    this.gradId = '';
+    this.brandiId = '';
+    this.gradDate = '';
+    this.candidate = '';
+  }
+}
